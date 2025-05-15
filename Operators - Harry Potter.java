8e7203cc44ddf097049a10b8a27ddf52d67f9317
Mohanavelayyavu <168104003+Mohanavelayyavu@users.noreply.github.com>
2025-05-15 21:20:46 +0530

    Update Operators - Harry Potter.java

--- Operators - Harry Potter.java
+++ Operators - Harry Potter.java
@@ -4,20 +4,12 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine().trim();
-
-        if(input.startsWith("-") || input.startsWith("+")) {
+        if (input.startsWith("-") || input.startsWith("+")) {
             input = input.substring(1);
         }
-
-        char firstDigitChar = input.charAt(0);
-        char lastDigitChar = input.charAt(input.length() - 1);
-
-        int firstDigit = firstDigitChar - '0';
-        int lastDigit = lastDigitChar - '0';
-
-        int sum = firstDigit + lastDigit;
-        System.out.println(sum);
-
+        int firstDigit = input.charAt(0) - '0';
+        int lastDigit = input.charAt(input.length() - 1) - '0';
+        System.out.println(firstDigit + lastDigit);
         scanner.close();
     }
 }

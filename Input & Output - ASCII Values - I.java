4d0376d3a0f7e18846a946b24c33a6eaa02d7c5d
Mohanavelayyavu <168104003+Mohanavelayyavu@users.noreply.github.com>
2025-05-15 21:03:42 +0530

    Create Input & Output - ASCII Values - I.java

---
+++ Input & Output - ASCII Values - I.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        char ch = scanner.next().charAt(0);
+
+       
+        int ascii = (int) ch;
+        System.out.println(ascii);
+
+        scanner.close();
+    }
+}
